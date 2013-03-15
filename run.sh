1a0cd888268aec008397f401530b84ee4b106bad
Anders Hassis <anders.hassis@gmail.com>
2013-03-15 17:16:47 +0100

    Changed run script

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ for n in 1000 10000 100000 1000000 10000000 100000000
 do
 	echo "n = $n started"
 	echo "n = $n" >> $file
-	for t in 1 4 9 16
+	for t in 1 4 8 16 32
 	do
 		echo "t = $t" >> $file
         mpirun -hostfile nodes -mca plm_rsh_agent rsh -np $t qsort $n >> $file
